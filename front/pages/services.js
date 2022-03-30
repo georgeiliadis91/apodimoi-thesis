bb979ef619c27600896f735ffe27ec2af0a196b4
georgeiliadis91 <george.iliadis.91@gmail.com>
2022-03-30 23:31:54 +0300

    services

--- front/pages/services.js
+++ front/pages/services.js
@@ -3,19 +3,19 @@ import styles from "../styles/Services.module.css";
 
 const Services = ({ data }) => {
   if (!data) return null;
-
   return (
     <>
       <h1 className="pageTitle">Υπηρεσίες</h1>
       <div className={styles.serviceTable}>
-        {data.map((service) => {
+        {data.map(({ attributes, id }) => {
+          const { service_name, email, number } = attributes;
           return (
-            <div className={styles.serviceRow} key={service.id}>
+            <div className={styles.serviceRow} key={id}>
               <span className={`${styles.cell} ${styles.title}`}>
-                {service.service_tiltle}
+                {service_name}
               </span>
-              <span className={`${styles.cell}`}>{service.email}</span>
-              <span className={`${styles.cell}`}>{service.phone}</span>
+              <span className={`${styles.cell}`}>{email}</span>
+              <span className={`${styles.cell}`}>{number}</span>
             </div>
           );
         })}
